2fe7d13c0c8be85fe7ff6a0d097a33606e0197a0
lcorbo-cb <lcorbo@cloudbees.com>
2021-06-14 22:40:22 -0400

    prepping for demo

--- sh_test_env_orc_cbci_modern_lcorbo/teardown_environment.sh
+++ sh_test_env_orc_cbci_modern_lcorbo/teardown_environment.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-terraform -chdir=../tf_jenkins_team_masters destroy --auto-approve
-terraform -chdir=../tf_jenkins_managed_masters destroy --auto-approve
-terraform -chdir=../tf_jenkins_cjoc destroy --auto-approve
+# terraform -chdir=../tf_jenkins_team_masters destroy --auto-approve
+# terraform -chdir=../tf_jenkins_managed_masters destroy --auto-approve
+# terraform -chdir=../tf_jenkins_cjoc destroy --auto-approve
 terraform -chdir=../tf_gcp_helm_cbci apply --auto-approve
 terraform -chdir=../tf_gcp_helm_cbci destroy --auto-approve
-#terraform -chdir=../tf_gcp_gke_private destroy --auto-approve
+terraform -chdir=../tf_gcp_gke_private destroy --auto-approve
